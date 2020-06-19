f0df9927382411256e4ed17882a0fa91bcdc9fb8
Dkumar8-adm <Dkumar8-adm@ULVMCTMTTL104.INTERNAL.COLT.NET>
2020-06-19 10:51:58 +0530

    update

--- APTAutomationProject/src/com/saksoft/qa/driverlibrary/XMLReader.java
+++ APTAutomationProject/src/com/saksoft/qa/driverlibrary/XMLReader.java
@@ -7,29 +7,25 @@ import org.dom4j.DocumentException;
 import org.dom4j.io.SAXReader;
 
 public class XMLReader {
-	
+
 	String fileloaction;
-	public XMLReader(String filepath)
-	{
-		fileloaction=filepath;
+
+	public XMLReader(String filepath) {
+		fileloaction = filepath;
 	}
-	
-public String getlocator(String nodepath) throws DocumentException, InterruptedException
-{
-	String locator=null;
-	File Inputfile=new File(fileloaction);
-	SAXReader xmlreader=new SAXReader();
-	Document doc=xmlreader.read(Inputfile);
-	Thread.sleep(3000);
-	//Document doc= 'E:\APT_Supraja_Workspace\APT_Automation_Demo_2\src\com\saksoft\qa\pagerepository\APT_MCS_CreateOrder_NGIN_ServiceType.xml';
-	locator=doc.selectSingleNode(nodepath).getText();
-	Log.info(locator);
-	return locator;
-	
-}
 
+	public String getlocator(String nodepath) throws DocumentException, InterruptedException {
+		String locator = null;
+		File Inputfile = new File(fileloaction);
+		SAXReader xmlreader = new SAXReader();
+		Document doc = xmlreader.read(Inputfile);
+		Thread.sleep(3000);
+		// Document doc=
+		// 'E:\APT_Supraja_Workspace\APT_Automation_Demo_2\src\com\saksoft\qa\pagerepository\APT_MCS_CreateOrder_NGIN_ServiceType.xml';
+		locator = doc.selectSingleNode(nodepath).getText();
+		Log.info(locator);
+		return locator;
 
+	}
 
 }
-
-

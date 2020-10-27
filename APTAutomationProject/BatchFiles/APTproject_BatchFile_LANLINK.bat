d47c253eff597f703e6f0519d1a59ee4d2435549
SKathiresan-Adm <SKathiresan-Adm@NAVMCTMTTL108.INTERNAL.COLT.NET>
2020-10-27 15:05:58 +0530

    adding Suite and Batch files

---
+++ APTAutomationProject/BatchFiles/APTproject_BatchFile_LANLINK.bat
@@ -0,0 +1,10 @@
+@Echo off
+cd..
+SET LOGFILE2=D:\APT_Phase1_Automation_Project_Deliverables\APTAutomationProject\Logs\consoleLogger.log
+call :Logit >> %LOGFILE2%
+exit /b 0
+:Logit
+set projectpath=D:\APT_Phase1_Automation_Project_Deliverables\APTAutomationProject
+set classpath=%projectpath%\bin;%projectpath%\lib\*;%projectpath%\reportng\*
+java org.testng.TestNG Suite\LANLINK.xml
+ 

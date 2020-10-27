e0b6147d0b8a9f75a9434e5713e75be6bfcfb1b3
SKathiresan-Adm <SKathiresan-Adm@NAVMCTMTTL108.INTERNAL.COLT.NET>
2020-10-27 15:03:24 +0530

    Removed suite and bacth files

--- APTAutomationProject/BatchFiles/APTproject_BatchFile_Wholesale.bat
+++
@@ -1,9 +0,0 @@
-@Echo off
-cd..
-SET LOGFILE2=D:\APT_Phase1_Automation_Project_Deliverables\APTAutomationProject/Logs/consoleLogger.log
-call :Logit >> %LOGFILE2%
-exit /b 0
-:Logit
-set projectpath=D:\APT_Phase1_Automation_Project_Deliverables\APTAutomationProject
-set classpath=%projectpath%\bin;%projectpath%\lib\*;%projectpath%\reportng\*
-java org.testng.TestNG Suite\wholesaleSIPtrunking.xml 
